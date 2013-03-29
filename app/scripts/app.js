773141508bc84bb2de18a86b77923c4c75505c1a
Jose Jesus Perez Aguinaga <jj.perez.aguinaga@gmail.com>
2013-03-28 22:57:06 -0600

    Added sin based angle function

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -37,13 +37,16 @@ var baseContent = rsr.ellipse(x1, containerHeight, containerWidth-spaceForConten
 var x1 = 270;
 var y1 = 80;
 
-var containerWidth = 50;
+var containerWidth = 55;
 var containerHeight = 250;
 var skewHeight = 20;
 var spaceForContent = 4;
 
 var contentHeight = 120;
-var angle = 15;
+var contentWidth = containerWidth;
+var angle = 0;
+var m = containerHeight / contentWidth;
+console.log(m);
 
 var top = rsr.ellipse(x1, y1, containerWidth, skewHeight);
 var base = rsr.ellipse(x1, containerHeight, containerWidth+angle, skewHeight);
@@ -59,3 +62,30 @@ rsr.path("M"+(x1-containerWidth+spaceForContent)+","+ +(containerHeight-contentH
 //var content = rsr.rect(x1-containerWidth+spaceForContent,containerHeight-contentHeight,containerWidth*2-(spaceForContent*2),contentHeight).attr({fill: "rgba(5, 62, 123, 1)","stroke-width": 1});
 var topContent = rsr.ellipse(x1, containerHeight-contentHeight, containerWidth-spaceForContent, skewHeight-spaceForContent).attr({fill: "rgba(47, 83, 123, .2)","stroke-width": 1});
 var baseContent = rsr.ellipse(x1, containerHeight, containerWidth-spaceForContent+angle, skewHeight-spaceForContent).attr({fill: "rgba(5, 62, 123, .2)","stroke-width": 1});
+
+
+var angle = 691;
+
+function getTanDeg(deg) {
+  if( (deg/90) > 1) {
+    var rad90 = 90 * Math.PI/180;
+    var rad = (deg- (90*parseInt(deg/90, 10))) * Math.PI/180;
+    return Math.sin(rad90) * parseInt(deg/90, 10) + Math.sin(rad);
+  } else {
+    var rad = deg * Math.PI/180;
+    return Math.sin(rad);
+  }
+}
+
+var x0 = 10;
+var y0 = 10;
+var rx0 = 20;
+var rx1 = 300;
+var h = 40;
+
+var rx0 = getTanDeg(angle) > 0 ? h * getTanDeg(angle) + 10 : 10;
+console.log(rx0);
+
+var top = rsr.path("M"+x0+","+y0+"L"+rx0+","+y0);
+var bot = rsr.path("M"+x0+","+ +(y0+h)+"L"+rx1+","+ +(y0+h) );
+var join = rsr.path("M"+rx0+","+y0+"L"+rx1+","+ +(y0+h) );
